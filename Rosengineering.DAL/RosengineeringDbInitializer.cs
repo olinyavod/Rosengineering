b81a6f9839efaf5486f1a6dac888422748f1b43e
Кирилл Журавлёв <olinyavod@live.com>
2017-01-15 00:05:37 +1000

    Implement tests for crudmanager

--- Rosengineering.DAL/RosengineeringDbInitializer.cs
+++ Rosengineering.DAL/RosengineeringDbInitializer.cs
@@ -1,7 +1,5 @@
 ﻿using System.Data.Entity;
-using System.Data.Entity.Migrations;
 using Rosengineering.DAL.Migrations;
-using Rosengineering.DAL.Models;
 
 namespace Rosengineering.DAL
 {
--- Rosengineering.Tests/ManagerTests/CrudManagerTests.cs
+++ Rosengineering.Tests/ManagerTests/CrudManagerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Threading.Tasks;
 using Autofac;
+using AutoMapper.QueryableExtensions.Impl;
 using NUnit.Framework;
 using Ploeh.AutoFixture;
 using Rosengineering.BusinessLogic;
@@ -113,6 +114,142 @@ namespace Rosengineering.Tests.ManagerTests
 			Assert.IsTrue(addResult.WithErrors);
 		}
 
-		
+		[Test]
+		public void UpdateExistsModel()
+		{
+			var model = Fixture.Create<TModel>();
+			var addResult = Manager.Add(model);
+			Assert.IsFalse(addResult.HasError, addResult.ErrorMessage);
+
+			var updateResult = Manager.Update(model);
+
+			Assert.IsFalse(updateResult.HasError, updateResult.ErrorMessage);
+			Assert.IsFalse(updateResult.WithErrors);
+			Assert.IsTrue(updateResult.IsValid);
+			Assert.IsEmpty(updateResult.FailPropeties);
+		}
+
+		[Test]
+		public async Task UpdateExistsModelAsync()
+		{
+			var model = Fixture.Create<TModel>();
+			var addResult = await Manager.AddAsync(model);
+			Assert.IsFalse(addResult.HasError, addResult.ErrorMessage);
+
+			var updateResult = await Manager.UpdateAsync(model);
+
+			Assert.IsFalse(updateResult.HasError, updateResult.ErrorMessage);
+			Assert.IsFalse(updateResult.WithErrors);
+			Assert.IsTrue(updateResult.IsValid);
+			Assert.IsEmpty(updateResult.FailPropeties);
+		}
+
+		[Test]
+		public void UpdateNullModel()
+		{
+			var updateResult = Manager.Update(null);
+
+			Assert.IsTrue(updateResult.HasError);
+			Assert.IsNotEmpty(updateResult.ErrorMessage);
+			Assert.IsTrue(updateResult.WithErrors);
+			Assert.IsTrue(updateResult.IsValid);
+			Assert.IsEmpty(updateResult.FailPropeties);
+		}
+
+		[Test]
+		public async Task UpdateNullModelAsync()
+		{
+			var updateResult = await Manager.UpdateAsync(null);
+
+			Assert.IsTrue(updateResult.HasError);
+			Assert.IsNotEmpty(updateResult.ErrorMessage);
+			Assert.IsTrue(updateResult.WithErrors);
+			Assert.IsTrue(updateResult.IsValid);
+			Assert.IsEmpty(updateResult.FailPropeties);
+		}
+
+		[Test]
+		public void GetExistsModel()
+		{
+			var model = Fixture.Create<TModel>();
+			var addResult = Manager.Add(model);
+			Assert.IsFalse(addResult.HasError, addResult.ErrorMessage);
+
+			var getResult = Manager.Get(model.Id);
+
+			Assert.IsFalse(getResult.HasError, getResult.ErrorMessage);
+			Assert.NotNull(getResult.Result);
+		}
+
+		[Test]
+		public async Task GetExistsModelAsync()
+		{
+			var model = Fixture.Create<TModel>();
+			var addResult = await Manager.AddAsync(model);
+			Assert.IsFalse(addResult.HasError, addResult.ErrorMessage);
+
+			var getResult = await Manager.GetAsync(model.Id);
+
+			Assert.IsFalse(getResult.HasError, getResult.ErrorMessage);
+			Assert.NotNull(getResult.Result);
+		}
+
+		[Test]
+		public void GetNotExistsModel()
+		{
+			var getResult = Manager.Get(-1);
+			Assert.IsFalse(getResult.HasError, getResult.ErrorMessage);
+			Assert.Null(getResult.Result);
+		}
+
+		[Test]
+		public async Task GetNotExistsModelAsync()
+		{
+			var getResult = await Manager.GetAsync(-1);
+			Assert.IsFalse(getResult.HasError, getResult.ErrorMessage);
+			Assert.Null(getResult.Result);
+		}
+
+		[Test]
+		public void DeleteExistsModel()
+		{
+			var model = Fixture.Create<TModel>();
+			var addResult = Manager.Add(model);
+			Assert.IsFalse(addResult.HasError, addResult.ErrorMessage);
+
+			var deleteResult = Manager.Delete(model.Id);
+
+			Assert.False(deleteResult.HasError, deleteResult.ErrorMessage);
+		}
+
+		[Test]
+		public async Task DeleteExistsModelAsync()
+		{
+			var model = Fixture.Create<TModel>();
+			var addResult = await Manager.AddAsync(model);
+			Assert.IsFalse(addResult.HasError, addResult.ErrorMessage);
+
+			var deleteResult = await Manager.DeleteAsync(model.Id);
+
+			Assert.False(deleteResult.HasError, deleteResult.ErrorMessage);
+		}
+
+		[Test]
+		public void DeleteNotExistsModel()
+		{
+			var deleteResult =  Manager.Delete(-1);
+
+			Assert.IsTrue(deleteResult.HasError);
+		}
+
+		[Test]
+		public async Task DeleteNotExistsModelAsync()
+		{
+			var deleteResult = await Manager.DeleteAsync(-1);
+
+			Assert.IsTrue(deleteResult.HasError);
+		}
+
+
 	}
 }
